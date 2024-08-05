eff2db1e350afa51a9588c01a340a080e69aba3c
Gabriel Mello <gabrielpontemello1@gmail.com>
2024-08-05 09:48:40 -0300

    edit schedule

--- src/main/java/com/gabriel/pive/calendar/controllers/ScheduleController.java
+++ src/main/java/com/gabriel/pive/calendar/controllers/ScheduleController.java
@@ -5,10 +5,7 @@ import com.gabriel.pive.calendar.services.ScheduleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("schedule")
@@ -20,5 +17,13 @@ public class ScheduleController {
     @PostMapping
     public ResponseEntity<ScheduleDto> createSchedule(@RequestBody ScheduleDto dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.createSchedule(dto));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ScheduleDto> editSchedule(@PathVariable Long id, @RequestBody ScheduleDto dto){
+        if (scheduleService.editSchedule(id, dto) == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(scheduleService.editSchedule(id, dto));
     }
 }
--- src/main/java/com/gabriel/pive/calendar/services/ScheduleService.java
+++ src/main/java/com/gabriel/pive/calendar/services/ScheduleService.java
@@ -1,9 +1,12 @@
 package com.gabriel.pive.calendar.services;
 
 import com.gabriel.pive.calendar.dtos.ScheduleDto;
+import com.gabriel.pive.calendar.entities.Schedule;
 import com.gabriel.pive.calendar.repositories.ScheduleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ScheduleService {
@@ -13,6 +16,18 @@ public class ScheduleService {
 
     public ScheduleDto createSchedule(ScheduleDto dto){
         return ScheduleDto.toScheduleDto(scheduleRepository.save(dto.toSchedule()));
+    }
+
+    public ScheduleDto editSchedule(Long id, ScheduleDto dto){
+        Optional<Schedule> optionalSchedule = scheduleRepository.findById(id);
+        if (optionalSchedule.isEmpty()){
+            return null;
+        }
+        Schedule schedule = optionalSchedule.get();
+        schedule.setProcedureType(dto.procedureType());
+        schedule.setDate(dto.date());
+
+        return ScheduleDto.toScheduleDto(schedule);
     }
 
 }
