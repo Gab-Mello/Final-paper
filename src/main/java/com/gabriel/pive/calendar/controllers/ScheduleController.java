9191b694a8833a979620c532fcc4f48fb03e30c9
Gabriel Mello <gabrielpontemello1@gmail.com>
2024-08-05 11:21:23 -0300

    Schedule documentation

--- src/main/java/com/gabriel/pive/calendar/controllers/ScheduleController.java
+++ src/main/java/com/gabriel/pive/calendar/controllers/ScheduleController.java
@@ -2,6 +2,8 @@ package com.gabriel.pive.calendar.controllers;
 
 import com.gabriel.pive.calendar.dtos.ScheduleDto;
 import com.gabriel.pive.calendar.services.ScheduleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,16 +11,19 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("schedule")
+@Tag(name = "Schedule", description = "calendar management")
 public class ScheduleController {
 
     @Autowired
     private ScheduleService scheduleService;
 
+    @Operation(summary = "Save a new schedule", description = "It saves and returns a json with the new schedule")
     @PostMapping
     public ResponseEntity<ScheduleDto> createSchedule(@RequestBody ScheduleDto dto){
         return ResponseEntity.status(HttpStatus.CREATED).body(scheduleService.createSchedule(dto));
     }
 
+    @Operation(summary = "Edit a schedule by id", description = "It edits the schedule's data")
     @PutMapping("/{id}")
     public ResponseEntity<ScheduleDto> editSchedule(@PathVariable Long id, @RequestBody ScheduleDto dto){
         if (scheduleService.editSchedule(id, dto) == null){
@@ -27,6 +32,7 @@ public class ScheduleController {
         return ResponseEntity.status(HttpStatus.OK).body(scheduleService.editSchedule(id, dto));
     }
 
+    @Operation(summary = "Cancel a schedule by Id", description = "It cancels the schedule ")
     @DeleteMapping("/{id}")
     public ResponseEntity<ScheduleDto> cancelSchedule(@PathVariable Long id){
         if (scheduleService.cancelSchedule(id) == null){
