44e7cb1f47c998606d2a176814eacf046324ca4e
Gabriel Mello <gabrielpontemello1@gmail.com>
2024-08-09 19:59:31 -0300

    configuring

--- src/test/java/com/gabriel/pive/PiveApplicationTests.java
+++ src/test/java/com/gabriel/pive/PiveApplicationTests.java
@@ -2,7 +2,9 @@ package com.gabriel.pive;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class PiveApplicationTests {
 
