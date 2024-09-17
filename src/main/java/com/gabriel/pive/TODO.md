6b31b6654d0be96396fd64e14a2aab4f8f713d5e
Gabriel Mello <gabrielpontemello1@gmail.com>
2024-09-17 14:00:10 -0300

    returning formatted percentage

--- src/main/java/com/gabriel/pive/TODO.md
+++ src/main/java/com/gabriel/pive/TODO.md
@@ -1,6 +1,6 @@
 ## Backend
 - [x] Develop embryos transfer.
+- [x] Develop endpoint to get only available donors.
 - [ ] Delete receiver is getting error sometimes.
 - [ ] Develop pregnancy.
-- [ ] Develop endpoint to get only available donors.
 - [ ] Return percentage numbers with only 2 cases.
--- src/main/java/com/gabriel/pive/fiv/EmbryoProduction/dtos/ProductionSummaryDto.java
+++ src/main/java/com/gabriel/pive/fiv/EmbryoProduction/dtos/ProductionSummaryDto.java
@@ -2,13 +2,18 @@ package com.gabriel.pive.fiv.EmbryoProduction.dtos;
 
 import com.gabriel.pive.fiv.EmbryoProduction.entities.EmbryoProduction;
 
-public record ProductionSummaryDto(Integer totalEmbryos, Float embryosPercentage) {
+import java.text.DecimalFormat;
+
+public record ProductionSummaryDto(Integer totalEmbryos, String formattedPercentage) {
 
     public static ProductionSummaryDto toProductionSummaryDto(EmbryoProduction production){
         if (production == null){
             return null;
         }
+
+        DecimalFormat df = new DecimalFormat("#.00");
+        String formattedPercentage = df.format(production.getEmbryosPercentage()) + "%";
         return new ProductionSummaryDto(production.getTotalEmbryos(),
-                                        production.getEmbryosPercentage());
+                                        formattedPercentage);
     }
 }
