9bbde12de43ab1d7a732e4832980c4bf738c3912
Gabriel Mello <gabrielpontemello1@gmail.com>
2026-06-23 21:17:18 -0300

    chore(handlers): make handlers public

--- src/main/java/com/gabriel/pive/infra/GlobalExceptionHandler.java
+++ src/main/java/com/gabriel/pive/infra/GlobalExceptionHandler.java
@@ -19,67 +19,67 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RegistrationNumberAlreadyExistsException.class)
-    private ResponseEntity<String> registrationNumberAlreadyExists(RegistrationNumberAlreadyExistsException exception){
+    public ResponseEntity<String> registrationNumberAlreadyExists(RegistrationNumberAlreadyExistsException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(ReceiverCattleNotFoundException.class)
-    private ResponseEntity<String> receiverCattleNotFound(ReceiverCattleNotFoundException exception){
+    public ResponseEntity<String> receiverCattleNotFound(ReceiverCattleNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(BullNotFoundException.class)
-    private ResponseEntity<String> bullNotFound(BullNotFoundException exception){
+    public ResponseEntity<String> bullNotFound(BullNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(DonorCattleNotFoundException.class)
-    private ResponseEntity<String> donorNotFound(DonorCattleNotFoundException exception){
+    public ResponseEntity<String> donorNotFound(DonorCattleNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(FivNotFoundException.class)
-    private ResponseEntity<String> fivNotFound(FivNotFoundException exception){
+    public ResponseEntity<String> fivNotFound(FivNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(AllEmbryosAlreadyRegisteredException.class)
-    private ResponseEntity<String> allEmbryosAlreadyRegistered(AllEmbryosAlreadyRegisteredException exception){
+    public ResponseEntity<String> allEmbryosAlreadyRegistered(AllEmbryosAlreadyRegisteredException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(ProductionNotFoundException.class)
-    private ResponseEntity<String> cultivationNotFound(ProductionNotFoundException exception){
+    public ResponseEntity<String> cultivationNotFound(ProductionNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(EmbryoNotFoundException.class)
-    private ResponseEntity<String> embryoNotFound(EmbryoNotFoundException exception){
+    public ResponseEntity<String> embryoNotFound(EmbryoNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(OocyteCollectionAlreadyHasProduction.class)
-    private ResponseEntity<String> fivAlreadyHasCultivation(OocyteCollectionAlreadyHasProduction exception){
+    public ResponseEntity<String> fivAlreadyHasCultivation(OocyteCollectionAlreadyHasProduction exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(FivDoesNotHaveOocyteCollectionException.class)
-    private ResponseEntity<String> fivDoesNotHaveOocyteCollection(FivDoesNotHaveOocyteCollectionException exception){
+    public ResponseEntity<String> fivDoesNotHaveOocyteCollection(FivDoesNotHaveOocyteCollectionException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(ReceiverCattleAlreadyHasEmbryoException.class)
-    private ResponseEntity<String> receiverCattleAlreadyHasEmbryo(ReceiverCattleAlreadyHasEmbryoException exception){
+    public ResponseEntity<String> receiverCattleAlreadyHasEmbryo(ReceiverCattleAlreadyHasEmbryoException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(FivAlreadyHasOocyteCollectionException.class)
-    private ResponseEntity<String> fivAlreadyHasOocyteCollection(FivAlreadyHasOocyteCollectionException exception){
+    public ResponseEntity<String> fivAlreadyHasOocyteCollection(FivAlreadyHasOocyteCollectionException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(OocyteCollectionNotFoundException.class)
-    private ResponseEntity<String> oocyteCollectionNotFound(OocyteCollectionNotFoundException exception){
+    public ResponseEntity<String> oocyteCollectionNotFound(OocyteCollectionNotFoundException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
@@ -93,32 +93,32 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(ViableOocytesBiggerThanTotalException.class)
-    private ResponseEntity<String> viableOocytesBiggerThanTotal(ViableOocytesBiggerThanTotalException exception){
+    public ResponseEntity<String> viableOocytesBiggerThanTotal(ViableOocytesBiggerThanTotalException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(DonorAlreadyCollectedException.class)
-    private ResponseEntity<String> donorAlreadyCollected(DonorAlreadyCollectedException exception){
+    public ResponseEntity<String> donorAlreadyCollected(DonorAlreadyCollectedException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(InvalidDateException.class)
-    private ResponseEntity<String> invalidDate(InvalidDateException exception){
+    public ResponseEntity<String> invalidDate(InvalidDateException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 
     @ExceptionHandler(TransferNotFoundException.class)
-    private ResponseEntity<String> transferNotFound(TransferNotFoundException exception){
+    public ResponseEntity<String> transferNotFound(TransferNotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(InvalidNumberOfEmbryosException.class)
-    private ResponseEntity<String> invalidNumberOfEmbryos(InvalidNumberOfEmbryosException exception){
+    public ResponseEntity<String> invalidNumberOfEmbryos(InvalidNumberOfEmbryosException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(ReceiverCattleDoesNotHaveAnEmbryoException.class)
-    private ResponseEntity<String> receiverDoesNotHaveAnEmbryo(ReceiverCattleDoesNotHaveAnEmbryoException exception){
+    public ResponseEntity<String> receiverDoesNotHaveAnEmbryo(ReceiverCattleDoesNotHaveAnEmbryoException exception){
         return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
     }
 }
